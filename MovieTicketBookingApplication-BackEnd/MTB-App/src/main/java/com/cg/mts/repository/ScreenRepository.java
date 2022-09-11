82f33ab9088299c3d3e6f3ede0138a2045383644
PrachiDesai <prachidesai1311@gmail.com>
2022-09-11 12:48:11 +0530

    initial commit

---
+++ MovieTicketBookingApplication-BackEnd/MTB-App/src/main/java/com/cg/mts/repository/ScreenRepository.java
@@ -0,0 +1,9 @@
+package com.cg.mts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cg.mts.pojo.Screen;
+
+
+public interface ScreenRepository extends JpaRepository<Screen, Integer> {
+
+}
---
+++ MovieTicketBookingApplication-BackEnd/MTB-App/src/test/java/com/cg/mts/MovieFinalApplicationTests.java
@@ -0,0 +1,36 @@
+package com.cg.mts;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import com.cg.mts.controller.LoginController;
+import com.cg.mts.pojo.Booking;
+import com.cg.mts.pojo.Customer;
+import com.cg.mts.pojo.Movie;
+import com.cg.mts.pojo.Screen;
+import com.cg.mts.pojo.Seat;
+import com.cg.mts.pojo.Show;
+import com.cg.mts.pojo.Theatre;
+import com.cg.mts.pojo.Ticket;
+
+@SpringBootTest
+class MovieFinalApplicationTests extends AbstractTest {
+	@Override
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+	}
+
+	@Autowired
+	LoginController loginController;
+	
+}
